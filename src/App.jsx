3ceee7e99b5cfad7be7562ef0e7dcd74ead2704d
Oleksandr Eirikh <o.eyrikh@gmail.com>
2021-11-24 23:17:42 +0200

    Component Statistics

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import user from './components/Profile/user.json';
+import data from './components/Statistics/data.json';
 import Profile from './components/Profile/Profile';
+import Statistics from './components/Statistics/Statiatics';
+
 function App() {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics title="Upload Stats" stats={data} />
+      <Statistics stats={data} />
+    </div>
   );
 }
 
