c11a79769d8dcefdccdf04e2ac4a4f0f477a2441
Oleksandr Eirikh <o.eyrikh@gmail.com>
2022-02-06 13:20:39 +0200

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
-import user from './components/Profile/user.json';
+import {
+  username,
+  tag,
+  location,
+  avatar,
+  stats,
+} from './components/Profile/user.json';
 import data from './components/Statistics/data.json';
 import friends from './components/FriendList/ friends.json';
 import transactions from './components/TransactionHistory/transactions.json';
@@ -12,11 +18,11 @@ function App() {
   return (
     <div>
       <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
+        username={username}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+        stats={stats}
       />
       <Statistics title="Upload Stats" stats={data} />
       <Statistics stats={data} />
